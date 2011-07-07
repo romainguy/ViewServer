300902110056aa4ef3727ac3eb29cf64a5a3a34a
Romain Guy <romainguy@curious-creature.com>
2011-07-07 13:46:56 -0700

    Initial commit for ViewServer

---
+++ src/com/android/debug/hv/ViewServerActivity.java
@@ -0,0 +1,40 @@
+package com.android.debug.hv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ViewServerActivity extends Activity {
+	private int mCounter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        if (getIntent().getExtras() != null) mCounter = getIntent().getExtras().getInt("counter");
+        ((TextView) findViewById(R.id.label)).setText("Activity #" + (mCounter + 1));
+
+        ViewServer.get(this).addWindow(this);
+    }
+    
+    public void nextActivity(View v) {
+    	Intent intent = new Intent(this, getClass());
+    	intent.putExtra("counter", mCounter + 1);
+		startActivity(intent);
+    }
+    
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	ViewServer.get(this).removeWindow(this);
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	ViewServer.get(this).setFocusedWindow(this);
+    }
+}
